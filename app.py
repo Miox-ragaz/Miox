5467259391cf391a6db805951b31a296a61067f3
Miox-ragaz <ragazr40@gmail.com>
2026-01-09 18:37:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ HTML_TEMPLATE = '''
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mocat - تطبيق الدردشة الآمن</title>
-    
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+   
     <style>
         /* الأساسيات */
         * {
